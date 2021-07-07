7f2655ab491294bbf2fac26f2c3edfb657dd99e1
Demilululu <demilululu@gmail.com>
2021-07-07 17:29:45 +0800

    Add body-parser

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars',exphbs({defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-app.get('/', (req , res) => {
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(port, (req, res)=> {
-  console.log(`Express is listening to localhost:${port}`)
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`The Express server is running on http://localhost:${port}`)
 })
